e177b9889644192577f3ebab079a312492ac22ad
zsien <quezhiyong@deepin.org>
2024-11-22 15:03:42 +0800

    fix: 修复更新失败
    修复更新失败

--- src/ManagerAdaptor.cpp
+++ src/ManagerAdaptor.cpp
@@ -9,9 +9,31 @@ static const QString SYSTEMD1_MANAGER_PATH = "/org/freedesktop/systemd1";
 static const QString STATE_IDEL = "idle";
 static const QString STATE_UPGRADING = "upgrading";
 static const QString STATE_FAILED = "failed";
+static const QString STATE_SUCCESS = "success";
 
 static const QString OSTREE_REPO = "/sysroot/ostree/repo";
 static const QByteArray OSTREE_DEFAULT_REMOTE_NAME = "default";
+
+QDBusArgument &operator<<(QDBusArgument &argument, const Progress &progress)
+{
+    argument.beginStructure();
+    argument << progress.stage << progress.percent;
+    argument.endStructure();
+
+    return argument;
+}
+
+const QDBusArgument &operator>>(const QDBusArgument &argument, Progress &progress)
+{
+    argument.beginStructure();
+    argument >> progress.stage;
+    double d;
+    argument >> d;
+    progress.percent = d;
+    argument.endStructure();
+
+    return argument;
+}
 
 ManagerAdaptor::ManagerAdaptor(int upgradeStdoutFd, const QDBusConnection &bus, QObject *parent)
     : QObject(parent)
@@ -22,6 +44,9 @@ ManagerAdaptor::ManagerAdaptor(int upgradeStdoutFd, const QDBusConnection &bus, 
     , m_upgradable(false)
     , m_state(STATE_IDEL)
 {
+    qRegisterMetaType<Progress>("Progress");
+    qDBusRegisterMetaType<Progress>();
+
     m_server->listen(upgradeStdoutFd);
     connect(m_server, &QLocalServer::newConnection, this, [this] {
         auto *socket = m_server->nextPendingConnection();
@@ -33,33 +58,13 @@ ManagerAdaptor::ManagerAdaptor(int upgradeStdoutFd, const QDBusConnection &bus, 
             }
         });
     });
-
-    auto reply = m_systemdManager->GetUnit("dum-upgrade.service");
-    reply.waitForFinished();
-    if (!reply.isValid()) {
-        // todo: log
-        throw std::runtime_error("GetUnit dum-upgrade.service failed");
-    }
-
-    auto unitPath = reply.value();
-    m_dumUpgradeUnit = new org::freedesktop::systemd1::Unit(SYSTEMD1_SERVICE,
-                                                            unitPath.path(),
-                                                            QDBusConnection::systemBus());
-    m_bus.connect(SYSTEMD1_SERVICE,
-                  unitPath.path(),
-                  "org.freedesktop.DBus.Properties",
-                  "PropertiesChanged",
-                  this,
-                  SLOT(onDumUpgradeUnitPropertiesChanged(const QString &,
-                                                         const QVariantMap &,
-                                                         const QStringList &)));
 }
 
 ManagerAdaptor::~ManagerAdaptor() = default;
 
 void ManagerAdaptor::checkUpgrade(const QDBusMessage &message)
 {
-    if (m_state != STATE_IDEL || m_state != STATE_FAILED) {
+    if (m_state != STATE_IDEL && m_state != STATE_FAILED) {
         m_bus.send(message.createErrorReply(QDBusError::AccessDenied, "An upgrade is in progress"));
         return;
     }
@@ -98,40 +103,100 @@ void ManagerAdaptor::checkUpgrade(const QDBusMessage &message)
         return;
     }
 
-    auto remoteRefs = output.split('\n');
+    auto remoteRefs = output.trimmed().split('\n');
     if (remoteRefs.size() < 1) {
-        m_bus.send(message.createErrorReply(QDBusError::InternalError, "Check upgrade failed"));
+        m_bus.send(
+            message.createErrorReply(QDBusError::InternalError, "Check upgrade failed: no refs"));
         return;
     }
 
-    QString remoteCommit;
-    const auto ref = remoteRefs.last();
-    auto colonIdx = ref.indexOf(' ');
+    const auto lastRef = remoteRefs.last();
+    qWarning() << "lastRef" << lastRef;
+    auto colonIdx = lastRef.indexOf('\t');
     if (colonIdx == -1) {
+        m_bus.send(message.createErrorReply(QDBusError::InternalError,
+                                            "Check upgrade failed: refs format error"));
         return;
     }
 
-    auto branch = ref.sliced(OSTREE_DEFAULT_REMOTE_NAME.length() + 1, // "default:"
-                             colonIdx)
+    auto branch = lastRef.first(colonIdx)
+                      .sliced(OSTREE_DEFAULT_REMOTE_NAME.length() + 1) // "default:"
                       .trimmed();
-    auto commit = ref.sliced(colonIdx + 1).trimmed();
-    if (branch == OSTREE_DEFAULT_REMOTE_NAME) {
-        remoteCommit = currentCommit;
+    auto commit = lastRef.sliced(colonIdx + 1).trimmed();
+    qWarning() << "branch: " << branch << "commit: " << commit
+               << "currentCommit: " << currentCommit;
+    if (currentCommit == commit) {
+        if (m_upgradable == false) {
+            return;
+        }
+
+        m_upgradable = false;
+        emit upgradableChanged(m_upgradable);
+
         return;
     }
 
-    m_upgradable = currentCommit != remoteCommit;
+    m_remoteBranch = branch;
+    if (m_upgradable == true) {
+        return;
+    }
+    m_upgradable = true;
     emit upgradableChanged(m_upgradable);
+}
+
+static QString systemdEscape(const QString &str)
+{
+    auto tmp = str;
+    tmp.replace('-', "\\x2d");
+    tmp.replace('/', "-");
+
+    return tmp;
 }
 
 void ManagerAdaptor::upgrade(const QDBusMessage &message)
 {
+    QString version = OSTREE_DEFAULT_REMOTE_NAME + ':' + m_remoteBranch;
+    QString unit = QString("dum-upgrade@%1.service").arg(systemdEscape(version));
+
+    auto reply = m_systemdManager->LoadUnit(unit);
+    reply.waitForFinished();
+    if (!reply.isValid()) {
+        // todo: log
+        m_bus.send(message.createErrorReply(QDBusError::InternalError,
+                                            QString("GetUnit %1 failed").arg(unit)));
+        return;
+    }
+
+    auto unitPath = reply.value();
+    m_dumUpgradeUnit = new org::freedesktop::systemd1::Unit(SYSTEMD1_SERVICE,
+                                                            unitPath.path(),
+                                                            QDBusConnection::systemBus());
+
     auto activeState = m_dumUpgradeUnit->activeState();
     if (activeState == "active" || activeState == "activating" || activeState == "deactivating") {
         m_bus.send(message.createErrorReply(QDBusError::AccessDenied, "An upgrade is in progress"));
         return;
     }
+
+    m_bus.connect(SYSTEMD1_SERVICE,
+                  m_dumUpgradeUnit->path(),
+                  "org.freedesktop.DBus.Properties",
+                  "PropertiesChanged",
+                  this,
+                  SLOT(onDumUpgradeUnitPropertiesChanged(const QString &,
+                                                         const QVariantMap &,
+                                                         const QStringList &)));
     m_dumUpgradeUnit->Start("replace");
+}
+
+bool ManagerAdaptor::upgradable() const
+{
+    return m_upgradable;
+}
+
+QString ManagerAdaptor::state() const
+{
+    return m_state;
 }
 
 void ManagerAdaptor::onDumUpgradeUnitPropertiesChanged(const QString &interfaceName,
@@ -140,16 +205,37 @@ void ManagerAdaptor::onDumUpgradeUnitPropertiesChanged(const QString &interfaceN
 {
     if (interfaceName == "org.freedesktop.systemd1.Unit") {
         auto activeState = changedProperties.value("ActiveState").toString();
-        if (activeState == "active" || activeState == "activating"
-            || activeState == "deactivating") {
+        qWarning() << "activeState:" << activeState;
+        if (activeState == "active" || activeState == "activating") {
             m_state = STATE_UPGRADING;
+            emit stateChanged(m_state);
+        } else if (activeState == "deactivating") {
+            m_state = STATE_SUCCESS;
             emit stateChanged(m_state);
         } else if (activeState == "failed") {
             m_state = STATE_FAILED;
             emit stateChanged(m_state);
+
+            m_bus.disconnect(SYSTEMD1_SERVICE,
+                             m_dumUpgradeUnit->path(),
+                             "org.freedesktop.DBus.Properties",
+                             "PropertiesChanged",
+                             this,
+                             SLOT(onDumUpgradeUnitPropertiesChanged(const QString &,
+                                                                    const QVariantMap &,
+                                                                    const QStringList &)));
         } else {
             m_state = STATE_IDEL;
             emit stateChanged(m_state);
+
+            m_bus.disconnect(SYSTEMD1_SERVICE,
+                             m_dumUpgradeUnit->path(),
+                             "org.freedesktop.DBus.Properties",
+                             "PropertiesChanged",
+                             this,
+                             SLOT(onDumUpgradeUnitPropertiesChanged(const QString &,
+                                                                    const QVariantMap &,
+                                                                    const QStringList &)));
         }
     }
 }
